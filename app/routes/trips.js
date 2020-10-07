f31279b747f24befe58d0db855e848fe74a15183
Jennifer Louie <jennifer.louie1@t-mobile.com>
2020-10-07 04:49:02 -0700

    Add findTrip method

--- app/routes/trips.js
+++ app/routes/trips.js
@@ -7,15 +7,20 @@ const findTrip = (id) => {
 
 module.exports = function(app, db) {
 
-    app.post('/trips', (req, res) => {
-        console.log(req.body);
-        var newTrip = req.body;
-        trips.push(newTrip);
-        res.json(newTrip);
-    });
-
     app.get('/trips', (req, res) => {
         res.json(trips);
+    });
+
+    app.get('/trips/:tripId', (req, res) => {
+      const trip = findTrip(parseInt(req.params.tripId));
+      res.json(trip);
+    });
+
+    app.post('/trips', (req, res) => {
+      console.log(req.body);
+      var newTrip = req.body;
+      trips.push(newTrip);
+      res.json(newTrip);
     });
 
     app.put('trips/:tripId', (req, res) => {
